4721bde018b4727f35312717063fb65de3a2b738
Jon Lauridsen <mail@jonlauridsen.com>
2021-02-17 00:01:45 +0100

    add cancellation module

---
+++ src/cancellation.ts
@@ -0,0 +1,12 @@
+import AbortController from 'node-abort-controller'
+
+export const cancelController = new AbortController()
+export const cancelSignal = cancelController.signal
+
+export function cancelCheck (): void {
+  if (cancelSignal.aborted) throw new Error('dependency-cruising cancelled')
+}
+
+export function cancel (): void {
+  cancelController.abort()
+}
